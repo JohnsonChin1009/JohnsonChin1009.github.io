734fbf29b10d65cd65ea02264fccf5824c878b99
JohnsonChin1009 <johnsonchin1009@gmail.com>
2023-06-09 11:12:06 +0800

    Life changing commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/JohnsonChin1009.github.io' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/JohnsonChin1009.github.io' : '',
 
+  trailingSlash: true,
+
+  output: 'export',
 };
 
 module.exports = nextConfig;
