4b04d2142a374ea2c319371b82e4b9559db095b5
JohnsonChin1009 <johnsonchin1009@gmail.com>
2023-06-09 10:17:29 +0800

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/react-portfolio' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/react-portfolio/' : '',
-  // Replace exportPathMap with generateStaticPaths
-  generateStaticPaths: async () => {
-    return {
-      '/': { routes: ['/'] },
-      // Add other routes of your application here
-    };
-  },
+
 };
 
 module.exports = nextConfig;
